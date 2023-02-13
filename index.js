fdf933a8eb9120beb18a009a11681315987b120f
Vinicius <vmsilva1227@gmail.com>
2023-02-13 00:15:58 -0300

    learning how updates work in docker images

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Ola Docker!!"));
+app.get("/", (req, res) => res.send("Ola Docker!! Atualização!!"));
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
